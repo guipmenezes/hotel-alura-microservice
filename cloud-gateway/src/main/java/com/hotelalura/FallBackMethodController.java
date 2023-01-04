6c31e8b34831454cd647e60549e17a927db8a072
guipmenezes <guimenezesunb@gmail.com>
2023-01-04 17:32:05 -0300

    Feature: Implement the cloud gateway with Hystrix

---
+++ cloud-gateway/src/main/java/com/hotelalura/FallBackMethodController.java
@@ -0,0 +1,19 @@
+package com.hotelalura;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallBackMethodController {
+
+    @RequestMapping("/hospedeServiceFallBack")
+    public String hospedeServiceFallBackMethod() {
+        return "O sistema de hospede está demorando mais do que o esperado." + "Por favor tente novamente";
+    }
+
+    @RequestMapping("/fraudeServiceFallBack")
+    public String fraudeServiceFallBackMethod() {
+        return "O sistema de checkagem de fraude está demorando mais do que o esperado." + "Por favor tente novamente";
+    }
+}
--- hystrix-dashboard/src/main/java/com/hotelalura/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/hotelalura/HystrixDashboardApplication.java
@@ -3,13 +3,13 @@ package com.hotelalura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
 @EnableDiscoveryClient
-@EnableHystrix
-public class CloudGatewayApplication {
+public class HystrixDashboardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CloudGatewayApplication.class, args);
+        SpringApplication.run(HystrixDashboardApplication.class, args);
     }
 }
