d67efc6838b40469adf48e0b187757f02c3c14ce
guipmenezes <guimenezesunb@gmail.com>
2023-01-24 16:50:28 -0300

    Tests: Test the hospede repository custom method

---
+++ hospede/src/test/java/com/hotelalura/hospede/HospedeRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.hotelalura.hospede;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class HospedeRepositoryTest {
+
+    @Autowired
+    private HospedeRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void itShouldFindHospedeById() {
+        //given
+        long milis = System.currentTimeMillis();
+
+        Hospede hospede = new Hospede(
+                1,
+                "Guilherme",
+                "Menezes",
+                new java.sql.Date(milis),
+                "brasileiro",
+                "61983212365",
+                1
+        );
+
+        underTest.save(hospede);
+
+        //when
+        Hospede expected = underTest.findByHospedeId(1);
+
+        //then
+        assertThat(expected).isEqualTo(hospede);
+    }
+
+    @Test
+    void itShouldNotFindHospedeById() {
+        //given
+        Integer id = 1;
+
+        //when
+        Hospede expected = underTest.findByHospedeId(1);
+
+        //then
+        assertThat(expected).isNotEqualTo(id);
+    }
+}
