99f278995582f01b860765fbe12c69a522842e5f
guipmenezes <guimenezesunb@gmail.com>
2023-01-26 21:46:01 -0300

    Bug: Trying to resolve a test service bug

--- hospede/src/test/java/com/hotelalura/hospede/HospedeServiceTest.java
+++ hospede/src/test/java/com/hotelalura/hospede/HospedeServiceTest.java
@@ -7,21 +7,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.http.HttpResponse;
 import java.sql.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class HospedeServiceTest {
     @Mock
     private HospedeRepository hospedeRepository;
+    @Mock
     private RestTemplate restTemplate;
     private HospedeService underTest;
 
@@ -52,12 +61,12 @@ class HospedeServiceTest {
     }
 
     @Test
-    void registerHospede() {
+    void canRegisterHospede() {
         long milis = System.currentTimeMillis();
         HospedeRegistrationRequest request = new HospedeRegistrationRequest(
                 "Guilherme",
                 "Menezes",
-                new java.sql.Date(milis),
+                new Date(milis),
                 "brasileiro",
                 "6127812718"
         );
@@ -73,6 +82,11 @@ class HospedeServiceTest {
                 .build();
 
         //when
+        Mockito.when(restTemplate.postForObject("http://FRAUDE/fraude-check/{hospedeId}",
+                        request,
+                        ResponseEntity.class,
+                        hospede.getHospedeId())).thenReturn(new ResponseEntity(HttpStatus.OK));
+
         underTest.registerHospede(request);
 
         //then
