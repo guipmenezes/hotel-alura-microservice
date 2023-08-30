3f03bb8d6d94faab513d60ff7b3e75a13c55622f
guipmenezes <guimenezesunb@gmail.com>
2023-08-30 12:31:58 -0300

    Add: vadd validation to the reservation fields

--- reservas/src/main/java/com/hotelalura/Controller/ReservasController.java
+++ reservas/src/main/java/com/hotelalura/Controller/ReservasController.java
@@ -5,6 +5,9 @@ import DTO.ReservasRegistrationRequest;
 import com.hotelalura.Model.Reservas;
 import com.hotelalura.RabbitMQ.Service.RabbitMQService;
 import com.hotelalura.Service.ReservasService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,26 +32,26 @@ public class ReservasController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Reservas> findReservasById(@PathVariable("id") Integer reservasId) {
+    public ResponseEntity<Reservas> findReservasById(@PathVariable("id") @NotNull @Positive Integer reservasId) {
         return reservasService.findReservasById(reservasId);
     }
 
     @PostMapping
-    public ResponseEntity<Reservas> registerReserva(@RequestBody ReservasRegistrationRequest request) {
+    public ResponseEntity<Reservas> registerReserva(@RequestBody @Valid ReservasRegistrationRequest request) {
         log.info("novo registro de reserva {}", request);
         this.rabbitMQService.sendMessage(RabbitMQConstants.RESERVATION_QUEUE, request);
         return reservasService.registerReserva(request);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity updateReserva(@PathVariable("id") Integer reservaId,
+    public ResponseEntity updateReserva(@PathVariable("id") @NotNull @Positive Integer reservaId,
                                         @RequestBody ReservasRegistrationRequest request) {
         log.info("atualizando a reserva {}", request);
         return reservasService.updateReserva(reservaId, request);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteReserva(@PathVariable("id") Integer reservaId) {
+    public ResponseEntity<Void> deleteReserva(@PathVariable("id") @NotNull @Positive Integer reservaId) {
         log.info("Deletando a reserva...");
         return reservasService.deleteReserva(reservaId);
     }
--- reservas/src/main/java/com/hotelalura/Model/Reservas.java
+++ reservas/src/main/java/com/hotelalura/Model/Reservas.java
@@ -1,7 +1,13 @@
 package com.hotelalura.Model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Date;
 
@@ -15,9 +21,23 @@ public class Reservas {
     @SequenceGenerator(name = "reserva_id_sequence", sequenceName = "reserva_id_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reserva_id_sequence")
     private Integer reservasId;
+
+    @Column(name = "roomtype", length = 50, nullable = false)
+    @Pattern(regexp = "Quarto simples|Suíte|Suíte presidencial")
     private String roomType;
+
+    @Column(name = "checkin", nullable = false)
     private Date dataEntrada;
+
+    @Column(name = "checkout", nullable = false)
     private Date dataSaida;
+
+    @Column(name = "value", nullable = false)
+    @Min(50)
+    @Max(10000)
     private Double valor;
+
+    @Column(name = "paymentform", nullable = false)
+    @Pattern(regexp = "Boleto|Cartão de crédito|Cartão de débito|Pix")
     private String formaPagamento;
 }
